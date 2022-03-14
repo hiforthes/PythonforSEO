e6980c2d49d805e5f5cde0587fd42f0a0c0b2785
hiforthes <94062997+hiforthes@users.noreply.github.com>
2022-03-13 22:24:42 -0700

    variable names changed I made them better

--- 1-get-urls.py
+++ 1-get-urls.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://websiteurl.com/sitemap.xml'
-soup = BeautifulSoup(requests.get(url).content, 'lxml')
-allurls = soup.find_all("loc")
-xml_urls = [myurl.text for myurl in allurls]
+sitemapsoup = BeautifulSoup(requests.get(url).content, 'lxml')
+sitemapurls = sitemapsoup.find_all("loc")
+xml_urls = [myurl.text for myurl in sitemapurls]
 for websiteurls in xml_urls:
     print(websiteurls)
