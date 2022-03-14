42ddd997f05f1d7b187cdfffec0b8ea601c05d9e
hiforthes <94062997+hiforthes@users.noreply.github.com>
2022-03-13 22:21:55 -0700

    website url changed

--- 1-get-urls.py
+++ 1-get-urls.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://forthes.com/sitemap.xml'
+url = 'https://websiteurl.com/sitemap.xml'
 soup = BeautifulSoup(requests.get(url).content, 'lxml')
 allurls = soup.find_all("loc")
 xml_urls = [myurl.text for myurl in allurls]
